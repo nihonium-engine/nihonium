71f444a2d1e3f706d82d05b61327447b59596242
IDidMakeThat <icorpftw@outlook.com>
2018-06-01 10:54:13 +0100

    implemented filesystem directory mount

---
+++ source/filesystem/mount_directory.cpp
@@ -0,0 +1,54 @@
+#include "mount_directory.hpp"
+
+namespace nh {
+
+file_directory_t::file_directory_t(FILE* file) {
+  this->file = file;
+}
+
+file_directory_t::~file_directory_t() {
+  if (this->file) {
+    fclose(this->file);
+  }
+}
+
+void file_directory_t::seek_current(int offset) {
+  fseek(this->file, offset, SEEK_CUR);
+}
+
+void file_directory_t::seek_start(int offset) {
+  fseek(this->file, offset, SEEK_SET);
+}
+
+void file_directory_t::seek_end(int offset) {
+  fseek(this->file, -offset, SEEK_END);
+}
+
+size_t file_directory_t::read(size_t bytes, uint8_t* dest) {
+  return fread(dest, 1, bytes, this->file);
+}
+
+void file_directory_t::close() {
+  fclose(this->file);
+  this->file = nullptr;
+}
+
+bool file_directory_t::eof() {
+  return feof(this->file);
+}
+
+mount_directory_t::mount_directory_t(std::string base_path) {
+  this->base_path = base_path;
+}
+
+mount_directory_t::~mount_directory_t() {
+  
+}
+
+file_base_t* mount_directory_t::open_file(std::string path) {
+  FILE* stdio_file = fopen((this->base_path + path).c_str(), "rb");
+  file_directory_t* file = new file_directory_t(stdio_file);
+  return file;
+}
+
+}
---
+++ source/filesystem/mount_directory.hpp
@@ -0,0 +1,43 @@
+#ifndef NH_MOUNT_DIRECTORY_H
+#define NH_MOUNT_DIRECTORY_H
+
+#include "mount_base.hpp"
+
+namespace nh {
+
+class file_directory_t : public file_base_t {
+
+  public:
+  file_directory_t(FILE* file);
+  ~file_directory_t();
+
+  void seek_current(int offset) override;
+  void seek_start(int offset) override;
+  void seek_end(int offset) override;
+
+  size_t read(size_t bytes, uint8_t* dest) override;
+
+  void close() override;
+  bool eof() override;
+
+  private:
+  FILE* file;
+
+};
+
+class mount_directory_t {
+
+  public:
+  mount_directory_t(std::string base_path);
+  ~mount_directory_t();
+
+  file_base_t* open_file(std::string path);
+
+  private:
+  std::string base_path;
+
+};
+
+}
+
+#endif
