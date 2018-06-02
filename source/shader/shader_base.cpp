77afd11266d05f173a915154b28dcd6c01d98f09
IDidMakeThat <icorpftw@outlook.com>
2018-06-02 13:49:21 +0100

    fixed shader loading

--- source/shader/shader_base.cpp
+++ source/shader/shader_base.cpp
@@ -12,6 +12,9 @@ shader_base_t::shader_base_t(file_base_t* vert_reader, file_base_t* frag_reader)
   this->vert_source = new char[this->vert_source_length];
   this->frag_source = new char[this->frag_source_length];
 
+  vert_reader->seek_start(0);
+  frag_reader->seek_start(0);
+
   vert_reader->read(this->vert_source_length, (uint8_t*)vert_source);
   frag_reader->read(this->frag_source_length, (uint8_t*)frag_source);
 
