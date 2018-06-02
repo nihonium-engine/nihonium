1adab2f3290efbaca2079a064c4fd02fd28d08df
IDidMakeThat <icorpftw@outlook.com>
2018-06-02 12:40:25 +0100

    modified main to use new filesystem

--- source/main/main_linux.cpp
+++ source/main/main_linux.cpp
@@ -1,8 +1,6 @@
 
-#include "../file_reader/file_reader_stdio.hpp"
-#include "../file_reader/file_reader_nar.hpp"
-
 #include "../filesystem/filesystem.hpp"
+#include "../filesystem/mount_directory.hpp"
 #include "../filesystem/mount_nar.hpp"
 
 #include "../renderer/renderer_glfw_gl.hpp"
@@ -19,24 +17,26 @@ int main(int argc, char** argv) {
   (void)argc; (void)argv;
 
   nh::filesystem_t* filesystem = new nh::filesystem_t();
-  nh::mount_nar_t* mount_main = new nh::mount_nar_t("test.nar");
+  nh::mount_nar_t* mount_test = new nh::mount_nar_t("test.nar");
 
-  filesystem->add_mount("main", mount_main);
+  filesystem->add_mount("test", mount_test);
   
-  nh::file_base_t* file = filesystem->open_file("main:LICENCE.txt");
+  nh::file_base_t* file = filesystem->open_file("test:LICENCE.txt");
   char* x = new char[16];
   x[15] = '\0';
   file->read(15, (uint8_t*)x);
   printf("Test: %s\n", x);
 
+  nh::mount_directory_t* mount_main = new nh::mount_directory_t("./");
+
   nh::renderer_glfw_gl_t* renderer = new nh::renderer_glfw_gl_t();
 
   nh::model_factory_t* model_factory = new nh::model_factory_t();
 
-  nh::file_reader_stdio_t* reader = new nh::file_reader_stdio_t("test.nmdl");
+  nh::file_base_t* reader = filesystem->open_file("main:test.nmdl");
 
-  nh::file_reader_stdio_t* vert_reader = new nh::file_reader_stdio_t("vert.vert");
-  nh::file_reader_stdio_t* frag_reader = new nh::file_reader_stdio_t("frag.frag");
+  nh::file_base_t* vert_reader = filesystem->open_file("main:vert.vert");
+  nh::file_base_t* frag_reader = filesystem->open_file("main:frag.frag");
 
   nh::shader_factory_t* shader_factory = new nh::shader_factory_t();
 
